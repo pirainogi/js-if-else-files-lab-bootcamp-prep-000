1a3b9eb79355eeeb1de794dbd94c5a3ac910ca47
Gabbie Piraino <pirainogi@gmail.com>
2018-10-07 23:51:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var name = "Joe"
 var height = "74"
-
+if (name === "Joe")
+{
+  console.log("Susan")
+}
+if (height === "74")
+{
+  console.log("70")
+}
 // Don't worry about this
 module.exports = { name, height
 }
